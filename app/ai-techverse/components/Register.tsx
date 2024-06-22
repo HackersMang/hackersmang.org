d084cc27acf770209f4ee11b27ed592eea69d18d
Mashfooq <mashhussmashfoo@gmail.com>
2024-06-22 10:35:58 +0530

    FIxed scroll based velocity.

--- app/ai-techverse/components/Register.tsx
+++ app/ai-techverse/components/Register.tsx
@@ -1,3 +1,5 @@
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
+
 const Register = () => {
 
   return (
@@ -15,22 +17,11 @@ const Register = () => {
         <h3 className="text-2xl text-[#d4e20b]">RSVP Route</h3>
       </div>
       <div className="col-span-2 mt-2">
-        <div
-          className="w-full inline-flex flex-nowrap overflow-hidden p-4 bg-[#1d1d1c] [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
-        >
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-2 [&_li]:text-white animate-infinite-scroll">
-            <li className="font-light text-3xl">Tech</li>
-            <li className="font-light text-3xl">Event</li>
-            <li className="font-light text-3xl">Registration</li>
-            <li className="font-light text-3xl">Open.</li>
-          </ul>
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-2 [&_li]:text-white animate-infinite-scroll">
-            <li className="font-light text-3xl">Tech</li>
-            <li className="font-light text-3xl">Event</li>
-            <li className="font-light text-3xl">Registration</li>
-            <li className="font-light text-3xl">Open.</li>
-          </ul>
-        </div>
+        <VelocityScroll
+          text="Tech Event Registration Open."
+          default_velocity={3}
+          className="text-center font-light text-3xl md:text-5xl tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:leading-[5rem]"
+        />
       </div>
       <div className="col-span-2 m-2">
         <a href="https://maps.app.goo.gl/bB6HwqDftS4Nq1LT7" target="_blank" rel="noopener noreferrer" className="relative register-button-inner-shadow inline-flex items-center justify-center p-4 overflow-hidden pencil-studio italic text-3xl font-extrabold tracking-tighter bg-[#d4e20b] [&_li]:text-black rounded-2xl w-full h-full group view">
