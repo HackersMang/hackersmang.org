7cfa28355e83f129bfe58a03d4401b42e95af1c6
Mashfooq <mashhussmashfoo@gmail.com>
2024-07-04 19:04:10 +0530

    Fixes

--- app/ai-techverse/components/Hero.tsx
+++ app/ai-techverse/components/Hero.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Register from "./Register";
 import Venue from "./Venue";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -10,6 +11,7 @@ const Hero = () => {
       <div className="relative flex flex-1 flex-col nav-link-outline rounded-2xl gap-4 py-72 lg:py-60 flexCenter md:mx-2">
         <h1 className="text-5xl md:text-7xl lg:text-9xl text-[#9fb2b2] font-extrabold text-center">AI TECH<span className="text-[#d4e20b]">VERSE</span></h1>
         <p className="text-2xl text-[#9fb2b2] lg:text-3xl mx-3 text-center">Talks on cutting edge <span className="text-[#9fb2b2] whitespace-nowrap curved-undeline">AI technologies!</span></p>
+        <Link href={"#main-content"}>
         <Image
           src="/ai-techverse/chevron-down.svg"
           alt="Arrow down"
@@ -17,8 +19,9 @@ const Hero = () => {
           height={10}
           className="absolute bottom-20 h-5 w-5 animate-pulse self-center"
         />
+        </Link>
       </div>
-      <div className="relative flex flex-col gap-4 md:mx-2 rounded-3xl">
+      <div id="main-content" className="relative flex flex-col gap-4 md:mx-2 rounded-3xl">
         <Venue />
         <Register />
       </div>
--- components/hackersmang/Header.tsx
+++ components/hackersmang/Header.tsx
@@ -2,11 +2,11 @@ import Image from "next/image"
 
 const Header = () => {
     return (
-        <section className="grid grid-cols-4 lg:grid-cols-6 gap-1 md:gap-4 justify-center items-center outline outline-1 outline-gray-800">
-            <div className="col-start-2 col-end-3 flex justify-end items-center m-2 md:m-3">
+        <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-1 md:gap-4 justify-center items-center outline outline-1 outline-gray-800">
+            <div className="col-start-1 col-end-2 lg:col-start-2 lg:col-end-3 flex justify-end items-center m-2 md:m-3">
                 <Image src="/hackersmang-logo.jpg" alt="hackersmang-logo" width={75} height={75} className="rounded-full m-2 w-12 md:w-16" />
             </div>
-            <div className="col-end-5 col-span-3 lg:col-end-6 lg:col-span-3 flex justify-start items-center font-extrabold text-[22px] md:text-[44px] lg:text-5xl text-[#d4e20b] m-2 md:m-3">
+            <div className="col-end-5 col-span-3 md:col-span-4 lg:col-end-6 lg:col-span-3 flex justify-start items-center font-extrabold text-[22px] md:text-[44px] lg:text-5xl text-[#d4e20b] m-2 md:m-3">
                 Hackerspace Mangaluru
             </div>
         </section>
