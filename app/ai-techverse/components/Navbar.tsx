ebb05334caf74a0fbdf5a034ee325361abaebb6c
Mashfooq <mashhussmashfoo@gmail.com>
2024-07-04 15:13:43 +0530

    Fixed header link

--- app/ai-techverse/components/Navbar.tsx
+++ app/ai-techverse/components/Navbar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <nav className="flexCenter padding-container relative z-30 py-5">
-      <Link href="/" className="view pt-2">
+      <Link href="/ai-techverse" className="view pt-2">
         <Image src="/ai-techverse/ai-techverse.svg" alt="ai-techverse logo" width={120} height={45} className="w-40 h-auto" />
       </Link>
     </nav>
