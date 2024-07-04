fd4c8532c9af43bb34b59579c1d1e86bae467148
Mashfooq <mashhussmashfoo@gmail.com>
2024-07-04 16:03:16 +0530

    Added teitter and opengraph

--- components/hackersmang/EventDeatils.tsx
+++ components/hackersmang/EventDeatils.tsx
@@ -14,11 +14,11 @@ const EventDetails = () => {
     <div className="col-start-5 col-end-9 flex flex-col justify-left items-start m-2 md:m-3 max-h-[700px]">
       <h3 className="text-[#9b9b9b] text-2xl font-bold mb-2">Event Details</h3>
       <div className="overflow-y-auto text-[#9b9b9b] no-scrollbar max-h-[700px] text-lg flex flex-col gap-2 w-full">
-        {EVENT_IMAGES.map((image, index) => (
-          <div key={index} className="flex justify-center items-center">
+        {EVENT_IMAGES.map((image, idx) => (
+          <div key={idx} className="flex justify-center items-center">
             <Image
               src={image}
-              alt={`Event image ${index + 1}`}
+              alt={`Event image ${idx + 1}`}
               width={300}
               height={300}
               className="w-full h-auto rounded-xl"
