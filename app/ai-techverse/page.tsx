bb102f0112a78f7d39111ee86e66616e7ce5c9b8
Mashfooq <mashhussmashfoo@gmail.com>
2024-07-04 20:36:37 +0530

    Added metadata

--- app/ai-techverse/page.tsx
+++ app/ai-techverse/page.tsx
@@ -11,8 +11,58 @@ import Agendas from "./components/Agendas";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AI TECHVERSE",
-  description: "AI TECHVERSE - Talks on cutting edge AI technologies hosted by Hackerspace Mangaluru."
+  metadataBase: new URL('https://hackersmang.org'),
+  title: "AI TECHVERSE by Hackerspace Mangaluru",
+  description: "AI TECHVERSE - Talks on cutting edge AI technologies hosted by Hackerspace Mangaluru.",
+  generator: 'AI TECHVERSE',
+  applicationName: 'AI TECHVERSE',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'AI TECHVERSE', 'Open Source', 'Cloud Computing', 'DevOps', 'Testing', 'Big Data', 'Search Engines', 'RDBMS', 'NoSQL', 'NLP', 'Machine Learning', 'Artificial Intelligence', 'Deep Learning', 'Automation', 'Python', 'PHP', 'Node.js', 'Angular', 'React', 'Vue.js', 'HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'Docker', 'Kubernetes', 'SaltStack', 'Ansible', 'Linux', 'MySQL', 'PostgreSQL', 'MongoDB', 'Sphinx', 'Elasticsearch', 'Data Science', 'TensorFlow', 'PyTorch', 'Blockchain', 'Cybersecurity', 'IoT', 'Edge Computing', 'Quantum Computing', '5G', 'AR/VR', 'Robotics', 'Microservices', 'Serverless', 'GraphQL', 'Jenkins', 'Terraform', 'AWS', 'Azure', 'Google Cloud', 'CI/CD', 'Agile', 'Scrum', 'Kanban', 'JIRA', 'Git', 'GitHub', 'GitLab', 'Bitbucket', 'CI/CD', 'SaaS', 'PaaS', 'IaaS', 'Firebase', 'Heroku', 'API', 'REST', 'GraphQL', 'WebAssembly', 'Progressive Web Apps', 'Single Page Applications', 'Server-Side Rendering', 'Jamstack', 'Headless CMS', 'Contentful', 'Strapi', 'Sanity', 'Hugo', 'Jekyll', 'Gatsby', 'Next.js', 'Nuxt.js', 'Django', 'Flask', 'FastAPI', 'Spring Boot', 'Laravel', 'Ruby on Rails'
+  ],
+  authors: [{ name: 'Hackerspace' }, { name: 'Mangaluru', url: 'https://hackersmang.org' }],
+  creator: 'Mashfooq Hussain',
+  publisher: 'Hackerspace Mangaluru',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: 'AI TECHVERSE by Hackerspace Mangaluru',
+    description: 'AI TECHVERSE - Talks on cutting edge AI technologies hosted by Hackerspace Mangaluru.',
+    url: 'https://hackersmang.org/ai-techverse',
+    siteName: 'Hackerspace Mangaluru',
+    images: [
+      {
+        url: 'https://hackersmang.org/ai-techverse/cards/hero-card.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://hackersmang.org/ai-techverse/cards/hero-card.png',
+        width: 1800,
+        height: 1600,
+        alt: 'AI TECHVERSE',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'AI TECHVERSE by Hackerspace Mangaluru',
+    description: 'AI TECHVERSE - Talks on cutting edge AI technologies hosted by Hackerspace Mangaluru.',
+    images: 'https://hackersmang.org/ai-techverse/cards/hero-card.png'
+  },
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  bookmarks: ['https://hackersmang.org/ai-techverse'],
+  category: 'technology',
 };
 
 function page() {
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,58 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://hackersmang.org'),
   title: "Hackerspace Mangaluru",
   description: "Hackerspace Mangaluru - Hosts Talks & Workshops on latest Open Source technologies & works towards building a thriving tech ecosystem in Mangaluru.",
+  generator: 'Hackerspace Mangaluru',
+  applicationName: 'Hackerspace Mangaluru',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'Hackerspace Mangaluru', 'AI TECHVERSE', 'Open Source', 'Cloud Computing', 'DevOps', 'Testing', 'Big Data', 'Search Engines', 'RDBMS', 'NoSQL', 'NLP', 'Machine Learning', 'Artificial Intelligence', 'Deep Learning', 'Automation', 'Python', 'PHP', 'Node.js', 'Angular', 'React', 'Vue.js', 'HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'Docker', 'Kubernetes', 'SaltStack', 'Ansible', 'Linux', 'MySQL', 'PostgreSQL', 'MongoDB', 'Sphinx', 'Elasticsearch', 'Data Science', 'TensorFlow', 'PyTorch', 'Blockchain', 'Cybersecurity', 'IoT', 'Edge Computing', 'Quantum Computing', '5G', 'AR/VR', 'Robotics', 'Microservices', 'Serverless', 'GraphQL', 'Jenkins', 'Terraform', 'AWS', 'Azure', 'Google Cloud', 'CI/CD', 'Agile', 'Scrum', 'Kanban', 'JIRA', 'Git', 'GitHub', 'GitLab', 'Bitbucket', 'CI/CD', 'SaaS', 'PaaS', 'IaaS', 'Firebase', 'Heroku', 'API', 'REST', 'GraphQL', 'WebAssembly', 'Progressive Web Apps', 'Single Page Applications', 'Server-Side Rendering', 'Jamstack', 'Headless CMS', 'Contentful', 'Strapi', 'Sanity', 'Hugo', 'Jekyll', 'Gatsby', 'Next.js', 'Nuxt.js', 'Django', 'Flask', 'FastAPI', 'Spring Boot', 'Laravel', 'Ruby on Rails'
+  ],
+  authors: [{ name: 'Hackerspace' }, { name: 'Mangaluru', url: 'https://hackersmang.org' }],
+  creator: 'Mashfooq Hussain',
+  publisher: 'Hackerspace Mangaluru',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: 'Hackerspace Mangaluru',
+    description: 'Hackerspace Mangaluru - Hosts Talks & Workshops on latest Open Source technologies & works towards building a thriving tech ecosystem in Mangaluru.',
+    url: 'https://hackersmang.org',
+    siteName: 'Hackerspace Mangaluru',
+    images: [
+      {
+        url: 'https://hackersmang.org/opengraph-image.jpeg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://hackersmang.org/opengraph-image.jpeg',
+        width: 1800,
+        height: 1600,
+        alt: 'Hackerspace Mangaluru',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Hackerspace Mangaluru',
+    description: 'Hackerspace Mangaluru - Hosts Talks & Workshops on latest Open Source technologies & works towards building a thriving tech ecosystem in Mangaluru.',
+    images: 'https://hackersmang.org/twitter-image.jpeg'
+  },
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  bookmarks: ['https://hackersmang.org'],
+  category: 'technology',
 };
 
 export default function RootLayout({
