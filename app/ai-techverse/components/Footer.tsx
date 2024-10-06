38b19020d004676d9cf57420bb4b84f88b35e8d3
Mashfooq <mashhussmashfoo@gmail.com>
2024-10-06 16:32:07 +0530

    Fixed issues

--- app/ai-techverse/components/Footer.tsx
+++ app/ai-techverse/components/Footer.tsx
@@ -2,7 +2,7 @@ import { WordFadeIn } from '@/components/magicui/word-fade-in'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='pb-8'>
       <WordFadeIn words="H A C K E R S M A N G" className="text-[2.5rem] md:text-7xl lg:text-[11.5rem] text-center mt-auto mb-2 lg:mb-0 outfit-black -space-x-2 lg:-space-x-9" delay={0.08} />
     </footer>
   )
