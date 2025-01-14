e09568b802d7954df84da1fed7c43e64d8321d1b
Mashfooq <mashhussmashfoo@gmail.com>
2025-01-14 16:27:43 +0530

    TechMang25 event changes

---
+++ app/techmang25/components/Venue.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Venue = () => {
+    return (
+        <div className="nav-link-outline rounded-3xl w-full flex flex-col items-center justify-center text-center mt-2">
+            <div className='p-8'>
+            <h3 className="text-2xl text-[#d4e20b] pt-3 mx-4">Happening On</h3>
+            <p className="w-full h-full p-4 text-lg md:text-xl">
+                Saturday <span className="text-[#9fb2b2] whitespace-nowrap">1st February, 2025</span>&nbsp;at&nbsp;<Link href="https://maps.app.goo.gl/bB6HwqDftS4Nq1LT7" target="_blank" rel="noopener noreferrer" className='underline'>UniCourt</Link> Mangaluru organised by&nbsp;<Link href="/" target="_blank" className='underline'>HackersMang</Link>.
+            </p>
+            </div>
+        </div>
+    );
+};
+
+export default Venue;
---
+++ app/techmang25/page.tsx
@@ -0,0 +1,86 @@
+import Footer from "../ai-techverse/components/Footer";
+import Hero from "./components/Hero";
+
+import "@/assets/ai-techverse.css"
+import "@/assets/fonts.css"
+// import Speakers from "./components/Speakers";
+// import Agendas from "./components/Agendas";
+import { Metadata } from "next";
+import Particles from "@/components/magicui/particles";
+import { WordFadeIn } from "@/components/magicui/word-fade-in";
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://hackersmang.org'),
+    title: "Hackers Mang | 2024 November edition by Hackerspace Mangaluru",
+    description: "Hackers Mang 2024 November edition hosted by Hackerspace Mangaluru.",
+    generator: 'Hackers Mang',
+    applicationName: 'Hackers Mang',
+    referrer: 'origin-when-cross-origin',
+    keywords: [
+        'Hackers Mang', 'Open Source', 'Cloud Computing', 'DevOps', 'Testing', 'Big Data', 'Search Engines', 'RDBMS', 'NoSQL', 'NLP', 'Machine Learning', 'Artificial Intelligence', 'Deep Learning', 'Automation', 'Python', 'PHP', 'Node.js', 'Angular', 'React', 'Vue.js', 'HTML5', 'CSS3', 'JavaScript', 'TypeScript', 'Docker', 'Kubernetes', 'SaltStack', 'Ansible', 'Linux', 'MySQL', 'PostgreSQL', 'MongoDB', 'Sphinx', 'Elasticsearch', 'Data Science', 'TensorFlow', 'PyTorch', 'Blockchain', 'Cybersecurity', 'IoT', 'Edge Computing', 'Quantum Computing', '5G', 'AR/VR', 'Robotics', 'Microservices', 'Serverless', 'GraphQL', 'Jenkins', 'Terraform', 'AWS', 'Azure', 'Google Cloud', 'CI/CD', 'Agile', 'Scrum', 'Kanban', 'JIRA', 'Git', 'GitHub', 'GitLab', 'Bitbucket', 'CI/CD', 'SaaS', 'PaaS', 'IaaS', 'Firebase', 'Heroku', 'API', 'REST', 'GraphQL', 'WebAssembly', 'Progressive Web Apps', 'Single Page Applications', 'Server-Side Rendering', 'Jamstack', 'Headless CMS', 'Contentful', 'Strapi', 'Sanity', 'Hugo', 'Jekyll', 'Gatsby', 'Next.js', 'Nuxt.js', 'Django', 'Flask', 'FastAPI', 'Spring Boot', 'Laravel', 'Ruby on Rails'
+    ],
+    authors: [{ name: 'Hackerspace' }, { name: 'Mangaluru', url: 'https://hackersmang.org' }],
+    creator: 'Mashfooq Hussain',
+    publisher: 'Hackerspace Mangaluru',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    openGraph: {
+        title: 'Hackers Mang | 2024 November edition by Hackerspace Mangaluru',
+        description: 'Hackers Mang | 2024 November edition hosted by Hackerspace Mangaluru.',
+        url: 'https://hackersmang.org/2024-november',
+        siteName: 'Hackerspace Mangaluru',
+        images: [
+            {
+                url: 'https://hackersmang.org/2024-november/cards/hero-card.png',
+                width: 800,
+                height: 600,
+            },
+            {
+                url: 'https://hackersmang.org/2024-november/cards/hero-card.png',
+                width: 1800,
+                height: 1600,
+                alt: 'AI TECHVERSE',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Hackers Mang 2024 November edition by Hackerspace Mangaluru',
+        description: 'Hackers Mang | 2024 November edition hosted by Hackerspace Mangaluru.',
+        images: 'https://hackersmang.org/2024-november/cards/hero-card.png'
+    },
+    alternates: {
+        canonical: '/',
+        languages: {
+            'en-US': '/en-US',
+        },
+    },
+    bookmarks: ['https://hackersmang.org/2024-november'],
+    category: 'technology',
+};
+
+function page() {
+    return (
+        <main className="relative bg-grainy">
+            <Particles
+                quantity={100}
+                ease={80}
+                color="#d4e20b"
+                refresh
+            />
+            <Hero />
+            {/* <Agendas /> */}
+            {/* <Speakers /> */}
+            <footer className='pt-24 pb-8'>
+                <WordFadeIn words="H A C K E R S M A N G" className="text-[2.5rem] text-center mt-auto mb-2 lg:mb-0 outfit-black -space-x-2" delay={0.08} />
+            </footer>
+        </main>
+    );
+}
+
+export default page
