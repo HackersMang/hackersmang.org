648b6a786061fcac1299cc443565319489d66a4e
Mashfooq <mashhussmashfoo@gmail.com>
2025-01-18 20:47:50 +0530

    TechMang25 event changes

--- app/techmang25/components/Hero.tsx
+++ app/techmang25/components/Hero.tsx
@@ -8,7 +8,7 @@ import About from "./About";
 
 const Hero = () => {
   return (
-    <section className="relative h-60 lg:h-80 w-full bg-grainy flex items-center justify-center">
+    <section className="relative h-60 lg:h-96 w-full bg-grainy flex items-center justify-center">
       <div className="flex flex-col items-center justify-center w-full px-4">
         <h1 className="text-5xl md:text-7xl lg:text-9xl text-[#9fb2b2] font-extrabold text-center transition-all">
           #TechMang25
