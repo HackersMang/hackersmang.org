1c3105c6aa47f28e571fe029da2f2bf4f10a4b46
Mashfooq <mashhussmashfoo@gmail.com>
2025-01-22 08:00:51 +0530

    TechMang25 event changes

--- app/ai-techverse/components/Hero.tsx
+++ app/ai-techverse/components/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = () => {
   return (
     <section className="flex flex-col gap-2 md:gap-4 pt-[6.5rem] pb-2 px-4">
       <div className="relative flex flex-1 flex-col nav-link-outline gap-4 py-72 lg:py-60 flexCenter md:mx-2">
-        <h1 className="text-5xl md:text-7xl lg:text-9xl text-[#9fb2b2] font-extrabold text-center">AI TECH<span className="text-[#d4e20b]">VERSE</span></h1>
-        <p className="text-2xl text-[#9fb2b2] lg:text-3xl mx-3 text-center">Talks on cutting edge <span className="text-[#9fb2b2] whitespace-nowrap curved-undeline">AI technologies!</span></p>
+        <h1 className="text-5xl md:text-7xl lg:text-9xl text-secondary font-extrabold text-center">AI TECH<span className="text-primary">VERSE</span></h1>
+        <p className="text-2xl text-secondary lg:text-3xl mx-3 text-center">Talks on cutting edge <span className="text-secondary whitespace-nowrap curved-undeline">AI technologies!</span></p>
         <Link href={"#main-content"}>
         <Image
           src="/ai-techverse/chevron-down.svg"
--- components/hackersmang/Header.tsx
+++ components/hackersmang/Header.tsx
@@ -14,8 +14,8 @@ export default function Header() {
 
   return (
     <>
-      <header className="m-2 mt-2 lg:m-6 lg:mb-1 p-0 outline outline-[1.5px] outline-[#2b2828] fixed top-0 left-0 right-0 z-50 bg-grainy">
-        <div className="flex items-center justify-between h-16">
+      <header className="p-2 pt-2 lg:p-6 lg:pb-1 m-0 fixed top-0 left-0 right-0 z-50 bg-grainy">
+        <div className="flex items-center justify-between h-16 outline outline-[1.5px] outline-[#2b2828]">
           {/* Logo Section */}
           <div className="h-full w-20 lg:w-32 flex justify-center items-center text-sm p-0 cursor-pointer">
             <Link href="/" passHref>
@@ -51,7 +51,7 @@ export default function Header() {
         className={`fixed inset-0 bg-grainy transform ${isMenuOpen ? 'translate-y-0' : '-translate-y-full'
           } transition-transform duration-300 ease-in-out z-40 pt-20`}
       >
-        <div className="flex flex-col items-center justify-center h-full space-y-8 text-3xl">
+        <div className="flex flex-col items-center justify-center h-full space-y-8 text-3xl text-secondary">
             <label className='font-medium text-base underline'>Upcoming Events</label>
             <Link href="/techmang25" className="hover:text-gray-300 transition-colors">
               <HyperText text="#TechMang25" className="outfit-bold text-gray-20 cursor-pointer" />
