9067216c4ec8a2233253a6322b449f794bd9bef0
Mashfooq <mashhussmashfoo@gmail.com>
2025-01-28 01:37:03 +0530

    Wireframe for TechMang25 displaying agenda #7

--- app/techmang25/components/Schedule.tsx
+++ app/techmang25/components/Schedule.tsx
@@ -10,9 +10,9 @@ const Schedule = () => {
     return (
         <section className="flex flex-col px-4 py-8 items-center justify-center">
             <h2 className="text-xl lg:text-2xl text-primary pb-4">Schedule</h2>
-            <h3 className="text-sm lg:text-xl pb-4 text-neutral">Track 1</h3>
+            <h3 className="text-md lg:text-xl pb-4 text-neutral">Track 1</h3>
             <ScheduleList speakers={sortedSpeakersTrack1} />
-            <h3 className="text-sm lg:text-xl py-4 lg:pt-8 text-neutral">Track 2</h3>
+            <h3 className="text-md lg:text-xl py-6 lg:pt-8 text-neutral">Track 2</h3>
             <ScheduleList speakers={sortedSpeakersTrack2} />
         </section>
     );
--- components/eventpage/ScheduleList.tsx
+++ components/eventpage/ScheduleList.tsx
@@ -15,7 +15,7 @@ const ScheduleList: React.FC<ScheduleListProps> = ({ speakers }) => {
                     className="flex flex-row items-center bg-[#1d1d1c] text-white rounded-lg shadow-lg p-4"
                 >
                     {/* Speaker Image */}
-                    <div className="flex-shrink-0 w-24 h-24 mb-2 lg:mb-0 lg:w-28 lg:h-28 relative">
+                    <div className="flex-shrink-0 w-24 h-24 lg:w-28 lg:h-28 relative">
                         <Image
                             src={speaker.image}
                             alt={speaker.name || 'Speaker'}
@@ -27,8 +27,8 @@ const ScheduleList: React.FC<ScheduleListProps> = ({ speakers }) => {
 
                     {/* Speaker Details */}
                     <div className="flex flex-col flex-grow pl-2 lg:pl-4">
-                        <h4 className="text-lg md:text-xl lg:text-2xl font-bold">{speaker.topic}</h4>
-                        <p className="text-sm md:text-base lg:text-lg text-neutral">{speaker.name}</p>
+                        <h4 className="text-md md:text-xl lg:text-2xl font-bold">{speaker.topic}</h4>
+                        <p className="text-sm md:text-md lg:text-lg text-neutral">{speaker.name}</p>
                     </div>
 
                     {/* Speaker Time */}
