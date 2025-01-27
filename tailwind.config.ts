733972bab6ec5bf6bbf859feaba43abb15403279
Mashfooq <mashhussmashfoo@gmail.com>
2025-01-28 01:48:03 +0530

    Wireframe for TechMang25 displaying agenda #7

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,19 +17,6 @@ module.exports = {
       },
       backgroundImage: {
         'grainy' : "url('/ai-techverse/noise.svg')",
-        pattern: "url('/pattern.png')",
-        'pattern-2': "url('/pattern-bg.png')",
-      },
-      screens: {
-        xs: '400px',
-        '3xl': '1680px',
-        '4xl': '2200px',
-      },
-      maxWidth: {
-        '10xl': '1512px',
-      },
-      borderRadius: {
-        '5xl': '40px',
       },
       animation: {
         'infinite-scroll': 'infinite-scroll 10s linear infinite',
