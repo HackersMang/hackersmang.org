cee8938ba20707c6259bdc59ec4b233bed043ee3
Mashfooq <mashhussmashfoo@gmail.com>
2025-04-06 16:40:56 +0530

    Removed Particle components, Performance improvement

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/hackersmang/Header";
 import Hero from "@/components/hackersmang/Hero";
 import "@/assets/fonts.css"
-import PerformanceParticles from "@/components/magicui/performance-particles";
 
 export default function Home() {
   return (
